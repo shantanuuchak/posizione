d8c249a89e83489de2e3bc8060c2874df23ddb4c
Shantanu Chakrawarty <51011922+shantanuuchak@users.noreply.github.com>
2024-10-07 15:13:47 +0530

    base api touch

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import { Hono } from "hono";
+
+const app = new Hono();
+
+app.get("/", (c) => {
+  return c.text("API Active!");
+});
+
+app.get("/current", async (c) => {
+  const data = await c.req.raw.cf;
+  console.log(data);
+  const locationData = {
+    city: data!.city,
+    provider: data!.asOrganization,
+    continent: data!.continent,
+    country: data!.country,
+    latitude: data!.latitude,
+    longitude: data!.longitude,
+    postalCode: data!.postalCode,
+    region: data!.region,
+    regionCode: data!.regionCode,
+    timezone: data!.timezone,
+    airport_code: data!.colo,
+  };
+  return c.json(locationData);
+});
+
+export default app;
